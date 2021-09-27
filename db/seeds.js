39bd8c1e98068be30aa401126e1173aaa5635250
Jo-Ivan Melgarejo <joivan.melgarejo@gmail.com>
2021-09-27 12:59:14 -0400

    copy seeds json in to seeds js

--- db/seeds.js
+++ db/seeds.js
@@ -1,9 +1,44 @@
 const posts = [
   {
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste quasi error a. Ipsum vero doloribus nesciunt rerum? Esse molestiae ipsam labore repudiandae quisquam accusamus placeat, nemo distinctio illum architecto velit?",
-    likes: 5,
-    dislikes: 2,
-    comments: ["This post sucks.", "Funny story!"]
+    text: "Today, I had to call patients that we hadn't seen in a long time. Most of them were no longer alive. That was kinda rough.",
+    likes: 25,
+    dislikes: 10,
+    comments: ["do you work for the VA", "sad", "at least some were alive", "hippa?", "sadge"]
+  },
+  {
+    text: "So I'll open Spotify on my phone and I'll go, 'Can I shuffle my songs, please?' And they go, 'No. It's the same order of songs and we'll never let you hear these 3 songs.' And I go, 'Okaaay!' And then I go to the bathroom. And then I come out of the bathroom and I go, 'Any updates?' And they go 'Yeah, we actually have four 30 second ads for you. Because we hate you. Now take this playlist we made based on your songs, go fetch!' And I go, 'Okaaay!' and I go over to the Spotify app and go, 'Can I change my album covers please?' and they go 'NO!' And I go, 'Okaaay!' And then I go over to the Spotify help desk, which is an oxymoron, and I go, 'Can I please listen to three songs without being interuptted?' and they go 'No! In fact, we're gonna frame you for murder, and you're gonna go to jail for thirty years!' And I go, 'Why are you doing this to me?!' And they go, 'Because we're Spotify",
+    likes: 106,
+    dislikes: 20,
+    comments: ["freemium experience", "bad UX dot png", "somehow it just keeps getting worse"]
+  },
+  {
+    text: "With Neopets turning pets into NFTs, I can only see more petsites following the trend, and really there's only a handful of big/popular ones... I don't want my dragons to turn into effing commodities. I know they're just collectible pixels and you'd think that would be perfect to tokenize, but it kind of strips all the heart and soul out of the whole experience of breeding dragons, writing lore for them, even just hatching a lucky egg... I'm scared that this stuff is going to spread.",
+    likes: 13,
+    dislikes: 12,
+    comments: ["what game op", "darned NFTs", "find a new game 5head"]
+  },
+  {
+    text: "I hate that I’m always trying to find cool biology themed stuff to wear but all the 'nature inspired' clothing companies just have like two crossed arrows or a minimalistic mountain on a sweatshirt. Effing lame, that’s barely even nature-adjacent. Put the life cycle of a salamander on a jacket, put hyena skeleton patterns on leggings, put a darn field guide of birds of prey on a peacoat and THEN you can have my money. Do NOT give me a shirt with a leaf on it that says 'stay wild' or some BS I would much prefer clothing that broadcasts to everyone around me how many teeth an adult Jaguar has or how some pitcher plants can catch and digest rats.",
+    likes: 30,
+    dislikes: 40,
+    comments: [
+      "nature clothing elitist? lol",
+      "i know right the state of clothing these days is terrible",
+      "The Absolute State Of Biology Clothing",
+      "go make your own clothes then"
+    ]
+  },
+  {
+    text: "Honestly if your anxiety is so bad that you can’t read like... something in the horror genre that I wrote (and tagged correctly) without tagging the post I made with 'this literally made me have an anxiety attack i’m panicking omg my dereality is acting up' maybe you SHOULDN’T INTERACT WITH ME, because I’m a horror fan, I’m very open about being a horror fan, and I don’t like being guilt tripped over and over about a harmless piece of 'creepy' writing I made!!",
+    likes: 225,
+    dislikes: 110,
+    comments: [
+      "fr u r doing it to urself",
+      "who tf with anxiety issues is reading horror",
+      "people with GAD deserve to enjoy the genre too. this is pretty closed minded op. :[",
+      "hate horror",
+      "you are in the wrong because horror is a terrible genre anyway lel"
+    ]
   }
 ];
 
